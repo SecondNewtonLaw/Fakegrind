59d574b978902b10449e7c561001ca647ee3867a
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2025-07-27 16:33:50 -0300

    test app (for now)

--- Fakegrind.CLI/main.cpp
+++ Fakegrind.CLI/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Sleep(5000);
+    printf("hi\n");
+    auto x = static_cast<uint64_t *>(malloc(sizeof(uint64_t) * 6000));
+    memset(x, 0xFF, sizeof(uint64_t) * 6000);
+    for (int i = 0; i < 6000; i++)
+        printf("%llu\n", x[i]);
+
     return 0;
 }
